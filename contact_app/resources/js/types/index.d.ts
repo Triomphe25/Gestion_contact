1e63640b5dd973b398410888ec895f102afd10ad
Triomphe25 <triomphembasa@gmail.com>
2025-09-28 00:49:36 +0200

    typage dans index.ts

--- contact_app/resources/js/types/index.d.ts
+++ contact_app/resources/js/types/index.d.ts
@@ -37,7 +37,39 @@ export interface User {
     avatar?: string;
     email_verified_at: string | null;
     two_factor_enabled?: boolean;
-    created_at: string;
+    created_at?: string;
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
+}
+
+export interface User {
+    id: number;
+    user_id: number;
+    first_name:string;
+    last_name:string;
+    email?:string;
+    phone?:string;
+    adress?:string;
+    city?:string;
+    postal_code?:string;
+    country?:string;
+    notes?:string;
+    created_at:string;
+    updated_at:string;
+}
+
+export interface PropsModels{
+    showModal: boolean;
+    modalType: "create"|"edit";
+    contact?: Contact;
+    onClose: ()=>void;
+}
+
+export interface PropsList{
+    contacts:Contact[];
+    onEdit:(contact: Contact)=>void;
+}
+export interface PropsSinglePage{
+    contact:Contact[];
+    onEdit:(contact: Contact)=>void;
 }
